cf271b50b34deae43d1bea27a6200e4d40a30bf7
tanish <tbhatia0225@gmail.com>
2024-10-09 21:24:37 +0530

    frontend with many features

---
+++ React-version/frontend/src/pages/Home.jsx
@@ -0,0 +1,191 @@
+import React, { useEffect, useState } from 'react'
+import { Link } from "react-router-dom"
+import { useDispatch } from 'react-redux';
+
+import HighlightText from '../components/core/HomePage/HighlightText'
+import CTAButton from "../components/core/HomePage/Button"
+import CodeBlocks from "../components/core/HomePage/CodeBlocks"
+import TimelineSection from '../components/core/HomePage/TimelineSection'
+import LearningLanguageSection from '../components/core/HomePage/LearningLanguageSection'
+import InstructorSection from '../components/core/HomePage/InstructorSection'
+import Footer from '../components/common/Footer'
+import ExploreMore from '../components/core/HomePage/ExploreMore'
+import ReviewSlider from '../components/common/ReviewSlider'
+import Course_Slider from '../components/core/Catalog/Course_Slider'
+
+import { getCatalogPageData } from '../services/operations/pageAndComponentData'
+
+import { MdOutlineRateReview } from 'react-icons/md'
+import { FaArrowRight } from "react-icons/fa"
+
+import { motion } from 'framer-motion'
+import { fadeIn, } from './../components/common/motionFrameVarients';
+
+// background random images
+import backgroundImg1 from '../assets/Images/random bg img/coding bg1.jpg'
+import backgroundImg2 from '../assets/Images/random bg img/coding bg2.jpg'
+import backgroundImg3 from '../assets/Images/random bg img/coding bg3.jpg'
+import backgroundImg4 from '../assets/Images/random bg img/coding bg4.jpg'
+import backgroundImg5 from '../assets/Images/random bg img/coding bg5.jpg'
+import backgroundImg6 from '../assets/Images/random bg img/coding bg6.jpeg'
+import backgroundImg7 from '../assets/Images/random bg img/coding bg7.jpg'
+import backgroundImg8 from '../assets/Images/random bg img/coding bg8.jpeg'
+import backgroundImg9 from '../assets/Images/random bg img/coding bg9.jpg'
+import backgroundImg10 from '../assets/Images/random bg img/coding bg10.jpg'
+import backgroundImg111 from '../assets/Images/random bg img/coding bg11.jpg'
+
+
+const randomImges = [
+    backgroundImg1,
+    backgroundImg2,
+    backgroundImg3,
+    backgroundImg4,
+    backgroundImg5,
+    backgroundImg6,
+    backgroundImg7,
+    backgroundImg8,
+    backgroundImg9,
+    backgroundImg10,
+    backgroundImg111,
+];
+
+// hardcoded
+
+
+
+const Home = () => {
+
+    // get background random images
+    const [backgroundImg, setBackgroundImg] = useState(null);
+
+    useEffect(() => {
+        const bg = randomImges[Math.floor(Math.random() * randomImges.length)]
+        setBackgroundImg(bg);
+    }, [])
+
+    // console.log('bg ==== ', backgroundImg)
+
+    // get courses data
+    const [CatalogPageData, setCatalogPageData] = useState(null);
+    const categoryID = "6506c9dff191d7ffdb4a3fe2" // hard coded
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        const fetchCatalogPageData = async () => {
+
+            const result = await getCatalogPageData(categoryID, dispatch);
+            setCatalogPageData(result);
+            // console.log("page data ==== ",CatalogPageData);
+        }
+        if (categoryID) {
+            fetchCatalogPageData();
+        }
+    }, [categoryID])
+
+
+    // console.log('================ CatalogPageData?.selectedCourses ================ ', CatalogPageData)
+
+
+    return (
+        <React.Fragment>
+            {/* background random image */}
+            <div>
+                <div className="w-full h-[450px] md:h-[650px] absolute top-0 left-0 opacity-[0.3] overflow-hidden object-cover ">
+                    <img src={backgroundImg} alt="Background"
+                        className="w-full h-full object-cover "
+                    />
+
+                    <div className="absolute left-0 bottom-0 w-full h-[250px] opacity_layer_bg "></div>
+                </div>
+            </div>
+
+            <div className=' '>
+                {/*Section1  */}
+                <div className='relative h-[450px] md:h-[550px] justify-center mx-auto flex flex-col w-11/12 max-w-maxContent items-center text-white '>
+
+                    <Link to={"/signup"}>
+                        <div className='z-0 group p-1 mx-auto rounded-full bg-richblack-800 font-bold text-richblack-200
+                                        transition-all duration-200 hover:scale-95 w-fit'>
+                            <div className='flex flex-row items-center gap-2 rounded-full px-10 py-[5px]
+                              transition-all duration-200 group-hover:bg-richblack-900'>
+                                <p>Become an Instructor</p>
+                                <FaArrowRight />
+                            </div>
+                        </div>
+
+                    </Link>
+
+                    <motion.div
+                        variants={fadeIn('left', 0.1)}
+                        initial='hidden'
+                        whileInView={'show'}
+                        viewport={{ once: false, amount: 0.1 }}
+                        className='text-center text-3xl lg:text-4xl font-semibold mt-7  '
+                    >
+                        Empower Your Future with
+                        <HighlightText text={"Scholar Station"} />
+                    </motion.div>
+
+                    <motion.div
+                        variants={fadeIn('right', 0.1)}
+                        initial='hidden'
+                        whileInView={'show'}
+                        viewport={{ once: false, amount: 0.1 }}
+                        className=' mt-4 w-[90%] text-center text-base lg:text-lg font-bold text-richblack-300'
+                    >
+                       
+                    </motion.div>
+
+
+                 
+                </div>
+
+                {/* animated code */}
+                <div className='relative mx-auto flex flex-col w-11/12 max-w-maxContent items-center text-white justify-between'>
+                    {/* Code block 1 */}
+                  
+
+                    {/* Code block 2 */}
+                   
+
+                    {/* course slider */}
+                    <div className='mx-auto box-content w-full max-w-maxContentTab px- py-12 lg:max-w-maxContent'>
+                        <h2 className='text-white mb-6 text-2xl '>
+                            Popular Picks for You 🏆
+                        </h2>
+                        <Course_Slider Courses={CatalogPageData?.selectedCategory?.courses} />
+                    </div>
+                    <div className=' mx-auto box-content w-full max-w-maxContentTab px- py-12 lg:max-w-maxContent'>
+                        <h2 className='text-white mb-6 text-2xl '>
+                            Top Enrollments Today 🔥
+                        </h2>
+                        <Course_Slider Courses={CatalogPageData?.mostSellingCourses} />
+                    </div>
+
+
+                    <ExploreMore />
+                </div>
+
+                {/*Section 2  */}
+             
+
+
+                {/*Section 3 */}
+                <div className='mt-14 w-11/12 mx-auto max-w-maxContent flex-col items-center justify-between gap-8 first-letter bg-richblack-900 text-white'>
+                    <InstructorSection />
+
+                    {/* Reviws from Other Learner */}
+                    <h1 className="text-center text-3xl lg:text-4xl font-semibold mt-8 flex justify-center items-center gap-x-3">
+                        Reviews from other learners <MdOutlineRateReview className='text-yellow-25' />
+                    </h1>
+                    <ReviewSlider />
+                </div>
+
+                {/*Footer */}
+                <Footer />
+            </div >
+        </React.Fragment>
+    )
+}
+
+export default Home
