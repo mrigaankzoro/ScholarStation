0d0506a3d1a64dc6bedeeb2977b07b6477c40fb8
Chaitanya Lohani <109726533+AloneFFOfficial@users.noreply.github.com>
2024-10-13 20:45:03 +0530

    Update course.js
    Basic setup for courses.js

---
+++ routes/course.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const router = express.Router();
+
+// Get course details by ID
+router.get("/:courseId", (req, res) => {
+    // handle getting course by ID
+});
+
+// Add new course
+router.post("/add", (req, res) => {
+    // handle adding new course
+});
+
+module.exports = router;
