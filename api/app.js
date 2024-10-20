31cf4c2c1ad3b33564495043576b74bb44bb4cbd
Chaitanya Lohani <chaitanyalohani175@gmail.com>
2024-10-20 09:18:38 +0530

    tried troubleshooting a few issues.

--- api/app.js
+++ api/app.js
@@ -1,7 +1,12 @@
-require("dotenv").config()
 const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
+6;
+
+/**
+ * Routes related to authentication.
+ * @module routes/auth
+ */
 const authRoutes = require("./routes/auth");
 const userRoutes = require("./routes/user");
 const courseRoutes = require("./routes/course");
@@ -13,23 +18,27 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-// MongoDB connection
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-   .then(() => console.log("Connected to MongoDB"))
-   .catch((err) => console.error("Could not connect to MongoDB", err));
+const uri = "mongodb://localhost:27017/ScholarStationDB";
+
+// Connect to MongoDB
+mongoose
+  .connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((err) => {
+    console.error("Could not connect to MongoDB:", err.message);
+    process.exit(1); // Optional: exit the process if connection fails
+  });
 
 // API routes
 app.use("/api/auth", authRoutes);
 app.use("/api/user", userRoutes);
 app.use("/api/courses", courseRoutes);
 
-app.get('/', (req, res) => {
-    res.send("Hello, I am Scholar Station API");
+app.get("/", (res) => {
+  res.send("Hello, I am Scholar Station API");
 });
 
 // Start the server
 app.listen(PORT, () => {
-    console.log(`App is listening on PORT: ${PORT}`);
+  console.log(`App is listening on PORT: ${PORT}`);
 });
-
-
